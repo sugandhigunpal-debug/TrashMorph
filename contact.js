800e19db922015b8695c5bda79608e0f8e9a6274
sugandhigunpal-debug <sugandhigunpal@gmail.com>
2025-12-10 20:46:24 +0530

    Add files via upload

---
+++ contact.js
@@ -0,0 +1,7 @@
+ document.addEventListener("DOMContentLoaded", () => {
+    const form = document.querySelector(".contact-form");
+
+    form.addEventListener("submit", () => {
+        alert("Your message has been sent!");
+    });
+});
